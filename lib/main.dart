5e13b2fde476cd3c790e58d712686708127b4f1b
Miles Krell <repos@mileskrell.com>
2019-07-27 17:48:02 -0400

    Move routes to separate files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 
-import 'resources.dart';
+import 'resources.dart' show pantone186;
+import 'route_home.dart' show HomeRoute;
+import 'route_onboarding.dart' show OnboardingRoute;
 
 void main() => runApp(MyApp());
 
@@ -21,75 +22,6 @@ class MyApp extends StatelessWidget {
         "/second": (context) => HomeRoute(appTitle)
       },
       initialRoute: "/",
-    );
-  }
-}
-
-class OnboardingRoute extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => OnboardingState();
-}
-
-class OnboardingState extends State<OnboardingRoute> {
-  var currentPageIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Onboarding page"),
-      ),
-      body: Stack(
-        children: <Widget>[
-          PageView(
-            onPageChanged: (newPageIndex) => setState(() {
-              currentPageIndex = newPageIndex;
-            }),
-            children: <Widget>[
-              Center(
-                  child: Text(
-                "Onboarding content",
-                style: bigTextStyle,
-              )),
-              Center(
-                  child: Text("More onboarding content", style: bigTextStyle)),
-              Center(
-                  child: RaisedButton(
-                child: Text("Go to main page", style: bigTextStyle),
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, "/second");
-                },
-              ))
-            ],
-          ),
-          Align(
-            alignment: Alignment(0, 0.75),
-            child: DotsIndicator(
-              dotsCount: 3,
-              position: currentPageIndex,
-              decorator: DotsDecorator(activeColor: pantone186),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class HomeRoute extends StatelessWidget {
-  final String title;
-
-  HomeRoute(this.title);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-          child: Text(
-              "This is the main page. If you try to go back, the app will close!")),
     );
   }
 }
