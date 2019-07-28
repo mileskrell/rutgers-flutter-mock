f9add74dd3b073d177f108b3786ebd11ef2be896
Miles Krell <repos@mileskrell.com>
2019-07-28 00:32:16 -0400

    HomeRoute: Add bottom nav bar + 4 pages

---
+++ lib/home_pages/page_my_apps.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyApps extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyAppsState();
+}
+
+class MyAppsState extends State<MyApps> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text(
+            "This is the My Apps page"));
+  }
+}
