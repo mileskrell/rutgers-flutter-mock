423ab041e483f80fbe6e1eded2b35c406e2a0564
Miles Krell <repos@mileskrell.com>
2019-07-28 00:45:58 -0400

    HomeRoute: Use current page title as app bar title

--- lib/route_home.dart
+++ lib/route_home.dart
@@ -7,6 +7,16 @@ import 'package:rutgers_basic_flutter_mock/home_pages/page_my_day.dart';
 import 'resources.dart';
 
 class HomeRoute extends StatefulWidget {
+  final bottomNavBarItems = [
+    BottomNavigationBarItem(
+        icon: Icon(Icons.calendar_today), title: Text("My Day")),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.person), title: Text("My Dashboard")),
+    BottomNavigationBarItem(icon: Icon(Icons.apps), title: Text("My Apps")),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.directions_bus), title: Text("Bus")),
+  ];
+
   @override
   State<StatefulWidget> createState() => HomeState();
 }
@@ -18,7 +28,7 @@ class HomeState extends State<HomeRoute> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(appTitle),
+        title: widget.bottomNavBarItems[currentPageIndex].title,
       ),
       body: <Widget>[
         MyDay(),
@@ -34,16 +44,7 @@ class HomeState extends State<HomeRoute> {
         onTap: (newIndex) => setState(() {
           currentPageIndex = newIndex;
         }),
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_today), title: Text("My Day")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person), title: Text("My Dashboard")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.apps), title: Text("My Apps")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.directions_bus), title: Text("Bus")),
-        ],
+        items: widget.bottomNavBarItems,
       ),
     );
   }
