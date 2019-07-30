c327c4d2becbd35a42694d79839b0cc20f676a24
Miles Krell <repos@mileskrell.com>
2019-07-29 20:44:04 -0400

    Fix My Day/My Dashboard padding and text alignment

--- lib/home_pages/page_my_day.dart
+++ lib/home_pages/page_my_day.dart
@@ -22,43 +22,44 @@ class MyDayState extends State<MyDay> {
         child: Text("My Day page for NetID user"),
       );
     }
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "Log in with NetID to view My Day",
-            style: bigTextStyle,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 16),
-          ),
-          RaisedButton(
-            child: Text("Log in with NetID", style: bigTextStyle),
-            onPressed: () async {
-              var loggedIn = await Navigator.push(context,
-                  MaterialPageRoute(builder: (context) {
-                return WebViewRoute(
-                    "https://cas.rutgers.edu/login?renew=true&service=https://my.rutgers.edu/portal/Login",
-                "Log in with NetID");
-              }));
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Log in with NetID to view My Day",
+              style: bigTextStyle,
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16),
+            ),
+            RaisedButton(
+              child: Text("Log in with NetID", style: bigTextStyle),
+              onPressed: () async {
+                var loggedIn = await Navigator.push(context,
+                    MaterialPageRoute(builder: (context) {
+                  return WebViewRoute(
+                      "https://cas.rutgers.edu/login?renew=true&service=https://my.rutgers.edu/portal/Login",
+                      "Log in with NetID");
+                }));
 
-              setState(() {
-                widget.appState.userType = UserType.NETID;
-              });
+                setState(() {
+                  widget.appState.userType = UserType.NETID;
+                });
 
-              Navigator.pushReplacementNamed(context, "/home");
-              () async {
-                final prefs = await SharedPreferences.getInstance();
-                prefs.setString("user_type", "net");
-                prefs.setBool("has_completed_tutorial", true);
-              }();
-            },
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 4),
-          ),
-        ],
+                Navigator.pushReplacementNamed(context, "/home");
+                () async {
+                  final prefs = await SharedPreferences.getInstance();
+                  prefs.setString("user_type", "net");
+                  prefs.setBool("has_completed_tutorial", true);
+                }();
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
