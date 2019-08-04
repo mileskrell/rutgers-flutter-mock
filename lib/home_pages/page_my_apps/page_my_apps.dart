73f1b4d9b25014a3abee201ca19d6ef630d80474
Miles Krell <mileskrell@gmail.com>
2019-08-04 18:40:29 -0400

    Add more documentation and another TODO

--- lib/home_pages/page_my_apps/page_my_apps.dart
+++ lib/home_pages/page_my_apps/page_my_apps.dart
@@ -7,11 +7,18 @@ import 'package:rutgers_flutter_mock/widgets/my_apps_app_widget.dart';
 
 /// The My Apps page. Currently shows different [App]s to each type of user.
 ///
+/// While on this page, a search button is included in the app bar. This filters
+/// the list of apps shown as the user types.
+///
 /// TODO: Display favorites and other apps separately.
 ///
 /// TODO: Probably all or almost all apps should be visible to everyone.
 ///  Different types of users should just have different *favorites* by default,
 ///  that's all.
+///
+/// TODO: Move all search bar-related logic out of HomeRoute. It should really
+///  be moved to either here or a third file. Does Flutter have anything like
+///  Android's [setHasOptionsMenu](https://tinyurl.com/hasOptionsMenu)?
 class MyApps extends StatelessWidget {
   final String searchText;
 
