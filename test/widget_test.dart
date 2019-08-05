ac9763497520b6d37767b06bca3c968328c522c0
Miles Krell <mileskrell@gmail.com>
2019-08-05 09:59:09 -0400

    Adjust commented-out test to satisfy linter

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,14 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+// TODO: Add tests
+/*
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:rutgers_flutter_mock/main.dart';
 
 void main() {
-  // TODO: Add tests
-  /*
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
@@ -29,5 +29,5 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
-  */
 }
+*/
