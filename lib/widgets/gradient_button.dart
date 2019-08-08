1683eeda566ab41c18df423fb97800ea351fef60
Miles Krell <mileskrell@gmail.com>
2019-08-08 09:54:12 -0400

    Improve gradient button; add login help
    The "square gradient button" can now also be used as a regular-size
    button. It's used like this in help buttons under "log in with ...ID".
    
    It's now also used as a square button in the place of some gray
    buttons on various other pages.

--- lib/widgets/gradient_button.dart
+++ lib/widgets/gradient_button.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:rutgers_flutter_mock/resources.dart';
 
-/// A square button. Used in both RoleSelectionRoute and OnboardingRoute.
-class SquareGradientButton extends StatelessWidget {
+/// A button with a diagonal gradient from [Colors.red] to [pantone186]
+class GradientButton extends StatelessWidget {
+  /// The text to display
   final String title;
+
+  /// What to do when the button is clicked
   final void Function() onPressed;
-  final double padding;
+
+  /// If true, shows a big square button instead of a small rectangular button
+  final bool isBigSquare;
+
+  /// How big should it be?
+  /// 130 by default (used for the role buttons). Sometimes it's good to have
+  /// it bigger, e.g. it's 160 on OnboardingLoginPage and 170 on
+  /// My Day and My Dashboard.
+  /// TODO: Is there a way of only allowing the text to wrap at spaces?
   final double size;
 
-  SquareGradientButton(
+  GradientButton(
       {@required this.title,
       @required this.onPressed,
-      this.padding = 8,
+      @required this.isBigSquare,
       this.size = 130});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(padding),
+      padding: EdgeInsets.all(8),
       child: RaisedButton(
         padding: EdgeInsets.all(0),
         onPressed: () => onPressed(),
@@ -32,12 +43,15 @@ class SquareGradientButton extends StatelessWidget {
             borderRadius: BorderRadius.circular(8),
           ),
           child: ConstrainedBox(
-            constraints: BoxConstraints.loose(Size.square(size)),
+            constraints: BoxConstraints.loose(
+                isBigSquare ? Size.square(size) : Size.fromWidth(size)),
             child: Center(
                 child: Padding(
-              padding: EdgeInsets.all(4),
+              padding: EdgeInsets.all(10),
               child: Text(title,
-                  style: bigTextStyle.copyWith(color: Colors.white),
+                  style: isBigSquare
+                      ? bigTextStyle.copyWith(color: Colors.white)
+                      : TextStyle(color: Colors.white),
                   textAlign: TextAlign.center),
             )),
           ),
