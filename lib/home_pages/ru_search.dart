438c41ff137fc9af38363b84d8c8e0d9c6dc092d
Miles Krell <repos@mileskrell.com>
2019-08-26 16:06:08 -0400

    HomeRoute: Add RU Search

---
+++ lib/home_pages/ru_search.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// The RU Search page (a Rutgers global search)
+class RuSearch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("RU Search content here"),
+    );
+  }
+}
